c43b13345a38609124db97a865972c54e0b3aa3b
jtorresli <manu.jose.torres@gmail.com>
2020-03-02 17:47:10 -0500

    feat: adding video responsive and fix menu burger

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const navSlide = () => {
     
     burger.addEventListener('click',() => {
         nav.classList.toggle('nav-active');
-
+        
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = ''
@@ -13,7 +13,7 @@ const navSlide = () => {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
             }
         });
-
+        
         burger.classList.toggle('toggle');
     });
 
